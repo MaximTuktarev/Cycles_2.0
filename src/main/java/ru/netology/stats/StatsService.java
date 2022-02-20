c288630f108b58cd479c6b64e0cc9a4910d126d9
MaximTuktarev <maxim.tuktarev@mail.ru>
2022-02-20 10:39:04 +0400

    init

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,68 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    public long CalculateSum(long[] sales) {
+        long sum = 0;
+        for (long sale : sales) {
+            sum = sum + sale;
+
+        }
+        return sum;
+
+    }
+
+    public long CalculateAverage(long[] sales) {
+        return CalculateSum(sales) / sales.length;
+
+
+    }
+
+
+    public int minSales(int[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+
+    public int maxSales(int[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+    public long BelowMedium(long[] sales) {
+        int months = 0;
+        for (long sale : sales)
+            if (sale <= CalculateAverage(sales)) {
+                months = months + 1;
+            }
+        return months;
+
+    }
+
+    public long AboveMedium(long[] sales) {
+        int months = 0;
+        for (long sale : sales)
+            if (sale >= CalculateAverage(sales)) {
+                months = months + 1;
+            }
+        return months;
+
+
+    }
+}
+
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,91 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+    @Test
+
+    public void ShouldCalculateAllSum() {
+
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 180;
+
+        long actual = service.CalculateSum(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void ShouldCalculateAverageSum() {
+
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 15;
+
+        long actual = service.CalculateAverage(sales);
+
+        assertEquals(expected, actual);
+
+
+    }
+
+
+    @Test
+    public void MinimumSales() {
+
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+
+        int actual = service.minSales(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void MaximumSales() {
+
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+
+        int actual = service.maxSales(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void FindBelowMed() {
+
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 7;
+
+        long actual = service.BelowMedium(sales);
+
+        assertEquals(expected, actual);
+
+
+    }
+    @Test
+
+    public void FindAboveMed() {
+
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 7;
+
+        long actual = service.AboveMedium(sales);
+
+        assertEquals(expected, actual);
+
+
+    }
+}
